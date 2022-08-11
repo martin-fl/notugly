55a978eafaab98a9533786fdeccb8040aff74ce5
martin-fl <martinfleurial@gmail.com>
2022-08-11 12:25:44 +0100

    renamed file and structs

--- src/format.rs
+++ src/format.rs
@@ -1,16 +1,27 @@
 use std::collections::VecDeque;
 
+/// Describes the different ways to assemble a document.
+///
+/// See pages 2 & 6 of [A prettier printer](https://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf),
 #[derive(Debug, Clone)]
 pub(crate) enum FormatDesc {
+    /// Does nothing, the empty word
     Nil,
+    /// Inserts a newline
     Line,
+    /// Inserts the given string
     Text(String),
+    /// Indent the given document by the given number of spaces
     Nest(i32, Box<FormatDesc>),
+    /// Concatenates the given documents
     Cat(Box<FormatDesc>, Box<FormatDesc>),
+    /// Represents a set of possible layouts. The two documents are required
+    /// to flatten to the same layout as an invariant.
     Union(Box<FormatDesc>, Box<FormatDesc>),
 }
 
 impl FormatDesc {
+    /// Removes indentation and replaces newlines with `c`.
     #[inline(always)]
     pub(crate) fn flatten_with(&self, c: &str) -> FormatDesc {
         match self {
@@ -25,22 +36,28 @@ impl FormatDesc {
         }
     }
 
+    /// Removes indentation and replaces newlines with a single space
     #[inline(always)]
     pub(crate) fn flatten(&self) -> FormatDesc {
         self.flatten_with(" ")
     }
 
+    /// Determines the best layout that fits within `w` columns, `k` of which being already used,
+    /// and transforms it into a [ProcessedFormat] for easier printing.
     #[inline(always)]
     pub(crate) fn best(self, w: i32, k: i32) -> ProcessedFormat {
         be(w, k, VecDeque::from([(0, self)]))
     }
 
+    /// Determines the best layout that fits within `w` columns,
+    /// and transforms it into a [ProcessedFormat] for easier printing.
     #[inline(always)]
     pub(crate) fn pretty(self, w: i32) -> ProcessedFormat {
         self.best(w, 0)
     }
 }
 
+/// Simplified representation of a formatted document
 #[derive(Debug, Clone)]
 pub enum ProcessedFormat {
     Nil,
@@ -49,6 +66,8 @@ pub enum ProcessedFormat {
 }
 
 impl std::fmt::Display for ProcessedFormat {
+    /// Corresponds to the `layout` function of
+    /// [A prettier printer](https://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf)
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             ProcessedFormat::Nil => Ok(()),
@@ -61,6 +80,7 @@ impl std::fmt::Display for ProcessedFormat {
 }
 
 impl ProcessedFormat {
+    /// Simply says if the documents fits in the remaining space or not.
     pub(crate) fn fits(&self, w: i32) -> bool {
         if w < 0 {
             return false;
@@ -73,6 +93,8 @@ impl ProcessedFormat {
     }
 }
 
+/// Given a list of (indentation, document) pairs, chooses the best layout possible for the given width `w`
+/// and remaining space `k`.
 pub(crate) fn be(w: i32, k: i32, mut z: VecDeque<(i32, FormatDesc)>) -> ProcessedFormat {
     match z.pop_front() {
         None => ProcessedFormat::Nil,
@@ -101,6 +123,7 @@ pub(crate) fn be(w: i32, k: i32, mut z: VecDeque<(i32, FormatDesc)>) -> Processe
     }
 }
 
+/// Returns `x` if it fits, othewise return `y`. Both documents are supposed to flatten to the same layout.
 #[inline(always)]
 pub(crate) fn better(w: i32, k: i32, x: ProcessedFormat, y: ProcessedFormat) -> ProcessedFormat {
     if x.fits(w - k) {
